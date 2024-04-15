fe912749cfe78a541e8d48e4accbaf1099ed43aa
Thomas <thomas.rubin2@web.de>
2024-04-16 01:40:05 +0200

    Activate debug, sync

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * The MIT License (MIT)
  *
  * Copyright (c) 2019 Ha Thach (tinyusb.org)
@@ -23,8 +23,8 @@
  *
  */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "bsp/board.h"
@@ -41,25 +41,23 @@ extern void hid_app_task(void);
 /*------------- MAIN -------------*/
 int main(void)
 {
-  board_init();
+    board_init();
 
-  printf("TinyUSB Host CDC MSC HID Example\r\n");
+    printf("TinyUSB Host CDC MSC HID Example\r\n");
 
-  // init host stack on configured roothub port
-  tuh_init(BOARD_TUH_RHPORT);
+    // init host stack on configured roothub port
+    tuh_init(BOARD_TUH_RHPORT);
 
-  while (1)
-  {
-    // tinyusb host task
-    tuh_task();
+    while(1)
+    {
+        // tinyusb host task
+        tuh_task();
 
-    led_blinking_task();
-    hid_app_task();
+        led_blinking_task();
+        hid_app_task();
+    }
 
-    //sleep_ms(1000);
-  }
-
-  return 0;
+    return 0;
 }
 
 //--------------------------------------------------------------------+
@@ -68,31 +66,33 @@ int main(void)
 
 void tuh_mount_cb(uint8_t dev_addr)
 {
-  // application set-up
-  printf("A device with address %d is mounted\r\n", dev_addr);
+    // application set-up
+    printf("A device with address %d is mounted\r\n", dev_addr);
 }
 
 void tuh_umount_cb(uint8_t dev_addr)
 {
-  // application tear-down
-  printf("A device with address %d is unmounted \r\n", dev_addr);
+    // application tear-down
+    printf("A device with address %d is unmounted \r\n", dev_addr);
 }
-
 
 //--------------------------------------------------------------------+
 // Blinking Task
 //--------------------------------------------------------------------+
 void led_blinking_task(void)
 {
-  const uint32_t interval_ms = 1000;
-  static uint32_t start_ms = 0;
+    const uint32_t interval_ms = 1000;
+    static uint32_t start_ms = 0;
 
-  static bool led_state = false;
+    static bool led_state = false;
 
-  // Blink every interval ms
-  if ( board_millis() - start_ms < interval_ms) return; // not enough time
-  start_ms += interval_ms;
+    // Blink every interval ms
+    if(board_millis() - start_ms < interval_ms)
+    {
+        return; // not enough time
+    }
+    start_ms += interval_ms;
 
-  board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
+    board_led_write(led_state);
+    led_state = 1 - led_state; // toggle
 }
